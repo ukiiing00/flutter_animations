9dd6e67754e35e83c35f928b6d96e0f369786a41
uk <ukiiing00@gmail.com>
2023-07-11 10:00:19 +0900

    MUSIC PLAYER PROJECT: Collection For ...

--- lib/screens/music_player_detail_screen.dart
+++ lib/screens/music_player_detail_screen.dart
@@ -46,6 +46,13 @@ class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
     ),
   );
 
+  late final AnimationController _menuController = AnimationController(
+    vsync: this,
+    duration: const Duration(
+      seconds: 5,
+    ),
+  );
+
   void _onPlayPauseTap() {
     if (_playPauseController.isCompleted) {
       _playPauseController.reverse();
@@ -71,189 +78,285 @@ class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen>
     _volume.value = _volume.value.clamp(0.0, size.width - 80);
   }
 
-  void _toggleMenu() {}
+  void _openMenu() {
+    _menuController.forward();
+  }
+
+  void _closeMenu() {
+    _menuController.reverse();
+  }
+
+  final List<Map<String, dynamic>> _menus = [
+    {
+      "icon": Icons.person,
+      "text": "Profile",
+    },
+    {
+      "icon": Icons.notifications,
+      "text": "Notifications",
+    },
+    {
+      "icon": Icons.settings,
+      "text": "Settings",
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Interstellar'),
-        actions: [
-          IconButton(
-            onPressed: _toggleMenu,
-            icon: const Icon(
-              Icons.menu,
+    return Stack(
+      children: [
+        Scaffold(
+          backgroundColor: Colors.black,
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            leading: IconButton(
+              icon: Icon(
+                Icons.close,
+                color: Colors.grey.shade200,
+              ),
+              onPressed: _closeMenu,
             ),
-          )
-        ],
-      ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 30,
           ),
-          Align(
-            child: Hero(
-              tag: widget.index,
-              child: Container(
-                height: 320,
-                width: 320,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                        color: Colors.black.withOpacity(0.4),
-                        blurRadius: 10,
-                        spreadRadius: 2,
-                        offset: const Offset(-2, 2))
-                  ],
-                  image: DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage("assets/covers/${widget.index}.jpeg"),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 15,
+            ),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 30,
+                ),
+                for (var menu in _menus) ...[
+                  Row(
+                    children: [
+                      Icon(
+                        menu['icon'],
+                        color: Colors.grey.shade200,
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        menu['text'],
+                        style: TextStyle(
+                          color: Colors.grey.shade200,
+                        ),
+                      )
+                    ],
                   ),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          AnimatedBuilder(
-            animation: _progressContrller,
-            builder: (context, child) {
-              return CustomPaint(
-                size: Size(size.width - 80, 5),
-                painter: ProgressBar(progressValue: _progressContrller.value),
-              );
-            },
-          ),
-          AnimatedBuilder(
-            animation: _progressContrller,
-            builder: (context, child) {
-              final seconds = Duration(
-                seconds: (_progressContrller.value * 60).floor(),
-              );
-              final secondsString = seconds.toString().split('.')[0].split(':');
-              return Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 40,
-                  vertical: 10,
-                ),
-                child: Row(
-                  children: [
-                    Text(
-                      '${secondsString[1]}:${secondsString[2]}',
-                      style: TextStyle(
-                        fontSize: 12,
-                        color: Colors.grey.shade500,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    const Spacer(),
-                    Text(
-                      '01:00',
-                      style: TextStyle(
-                        fontSize: 12,
-                        color: Colors.grey.shade500,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          const Text(
-            'Interstellar',
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          SizedBox(
-            height: 30,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 40,
-              ),
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  SlideTransition(
-                    position: _marquee,
-                    child: const Text(
-                      'A FILM By Christopher Nolan - Original Motion Picture SoundTrack',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                      ),
-                      maxLines: 1,
-                    ),
+                  const SizedBox(
+                    height: 30,
                   ),
                 ],
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          GestureDetector(
-            onTap: _onPlayPauseTap,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                AnimatedIcon(
-                  icon: AnimatedIcons.play_pause,
-                  progress: _playPauseController,
-                  size: 60,
+                const Spacer(),
+                Row(
+                  children: const [
+                    Icon(
+                      Icons.logout,
+                      color: Colors.red,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Log out",
+                      style: TextStyle(
+                        color: Colors.red,
+                      ),
+                    )
+                  ],
                 ),
-                // LottieBuilder.asset(
-                //   'assets/animations/play-button.json',
-                //   controller: _playPauseController,
-                //   width: 200,
-                //   height: 200,
-                //   onLoaded: (composition) {
-                //     _playPauseController.duration = composition.duration;
-                //   },
-                // )
+                const SizedBox(
+                  height: 100,
+                )
               ],
             ),
           ),
-          const SizedBox(
-            height: 30,
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: const Text('Interstellar'),
+            actions: [
+              IconButton(
+                onPressed: _openMenu,
+                icon: const Icon(
+                  Icons.menu,
+                ),
+              )
+            ],
           ),
-          GestureDetector(
-            onHorizontalDragUpdate: (details) => _onVolumeDragUpdate(details),
-            onHorizontalDragStart: (_) => _toggleDragging(),
-            onHorizontalDragEnd: (_) => _toggleDragging(),
-            child: AnimatedScale(
-              scale: isDragging ? 1.1 : 1,
-              duration: const Duration(milliseconds: 500),
-              curve: Curves.bounceOut,
-              child: Container(
-                clipBehavior: Clip.hardEdge,
-                decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(10)),
-                child: ValueListenableBuilder(
-                  valueListenable: _volume,
-                  builder: (context, value, child) => CustomPaint(
-                    size: Size(size.width - 80, 50),
-                    painter: VolumePainter(
-                      volume: value,
+          body: Column(
+            children: [
+              const SizedBox(
+                height: 30,
+              ),
+              Align(
+                child: Hero(
+                  tag: widget.index,
+                  child: Container(
+                    height: 320,
+                    width: 320,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black.withOpacity(0.4),
+                            blurRadius: 10,
+                            spreadRadius: 2,
+                            offset: const Offset(-2, 2))
+                      ],
+                      image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: AssetImage("assets/covers/${widget.index}.jpeg"),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
+              const SizedBox(
+                height: 50,
+              ),
+              AnimatedBuilder(
+                animation: _progressContrller,
+                builder: (context, child) {
+                  return CustomPaint(
+                    size: Size(size.width - 80, 5),
+                    painter:
+                        ProgressBar(progressValue: _progressContrller.value),
+                  );
+                },
+              ),
+              AnimatedBuilder(
+                animation: _progressContrller,
+                builder: (context, child) {
+                  final seconds = Duration(
+                    seconds: (_progressContrller.value * 60).floor(),
+                  );
+                  final secondsString =
+                      seconds.toString().split('.')[0].split(':');
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 40,
+                      vertical: 10,
+                    ),
+                    child: Row(
+                      children: [
+                        Text(
+                          '${secondsString[1]}:${secondsString[2]}',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey.shade500,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        const Spacer(),
+                        Text(
+                          '01:00',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey.shade500,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              const Text(
+                'Interstellar',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              SizedBox(
+                height: 30,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 40,
+                  ),
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      SlideTransition(
+                        position: _marquee,
+                        child: const Text(
+                          'A FILM By Christopher Nolan - Original Motion Picture SoundTrack',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          maxLines: 1,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              GestureDetector(
+                onTap: _onPlayPauseTap,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    AnimatedIcon(
+                      icon: AnimatedIcons.play_pause,
+                      progress: _playPauseController,
+                      size: 60,
+                    ),
+                    // LottieBuilder.asset(
+                    //   'assets/animations/play-button.json',
+                    //   controller: _playPauseController,
+                    //   width: 200,
+                    //   height: 200,
+                    //   onLoaded: (composition) {
+                    //     _playPauseController.duration = composition.duration;
+                    //   },
+                    // )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              GestureDetector(
+                onHorizontalDragUpdate: (details) =>
+                    _onVolumeDragUpdate(details),
+                onHorizontalDragStart: (_) => _toggleDragging(),
+                onHorizontalDragEnd: (_) => _toggleDragging(),
+                child: AnimatedScale(
+                  scale: isDragging ? 1.1 : 1,
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.bounceOut,
+                  child: Container(
+                    clipBehavior: Clip.hardEdge,
+                    decoration:
+                        BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                    child: ValueListenableBuilder(
+                      valueListenable: _volume,
+                      builder: (context, value, child) => CustomPaint(
+                        size: Size(size.width - 80, 50),
+                        painter: VolumePainter(
+                          volume: value,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
