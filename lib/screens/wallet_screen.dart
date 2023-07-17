288d84825b331adde008138cba9fe2aee817cd46
uk <ukiiing00@gmail.com>
2023-07-17 16:23:53 +0900

    WALLET PROJECT:  ...Trick / Animate Syntax

--- lib/screens/wallet_screen.dart
+++ lib/screens/wallet_screen.dart
@@ -8,6 +8,12 @@ class WalletScreen extends StatefulWidget {
   State<WalletScreen> createState() => _WalletScreenState();
 }
 
+List<Color> colors = [
+  Colors.black,
+  Colors.purple,
+  Colors.blue,
+];
+
 class _WalletScreenState extends State<WalletScreen> {
   bool _isExpanded = false;
 
@@ -36,35 +42,20 @@ class _WalletScreenState extends State<WalletScreen> {
           onTap: _onExpanded,
           child: Column(
             children: [
-              CreditCard(
-                bgColor: Colors.black,
-                isExpanded: _isExpanded,
-              )
-                  .animate(
-                    delay: 1.5.seconds,
-                    target: _isExpanded ? 0 : 1,
+              for (var idx in [0, 1, 2])
+                Hero(
+                  tag: idx,
+                  child: CreditCard(
+                    index: idx,
+                    isExpanded: _isExpanded,
                   )
-                  .flipV(end: 0.1),
-              CreditCard(
-                bgColor: Colors.purple,
-                isExpanded: _isExpanded,
-              )
-                  .animate(
-                    delay: 1.5.seconds,
-                    target: _isExpanded ? 0 : 1,
-                  )
-                  .flipV(end: 0.1)
-                  .slideY(end: -0.8),
-              CreditCard(
-                bgColor: Colors.blue,
-                isExpanded: _isExpanded,
-              )
-                  .animate(
-                    delay: 1.5.seconds,
-                    target: _isExpanded ? 0 : 1,
-                  )
-                  .flipV(end: 0.1)
-                  .slideY(end: -0.8 * 2),
+                      .animate(
+                        delay: 1.5.seconds,
+                        target: _isExpanded ? 0 : 1,
+                      )
+                      .flipV(end: 0.1)
+                      .slideY(end: -0.8 * idx),
+                ),
             ]
                 .animate(
                   interval: 500.milliseconds,
@@ -83,97 +74,198 @@ class _WalletScreenState extends State<WalletScreen> {
   }
 }
 
-class CreditCard extends StatelessWidget {
+class CreditCard extends StatefulWidget {
   const CreditCard({
     super.key,
-    required this.bgColor,
+    required this.index,
     required this.isExpanded,
   });
 
-  final Color bgColor;
+  final int index;
   final bool isExpanded;
 
+  @override
+  State<CreditCard> createState() => _CreditCardState();
+}
+
+class _CreditCardState extends State<CreditCard> {
   void _onTap() {
-    print("tapped");
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => CardDetailScreen(
+          index: widget.index,
+          isExpanded: widget.isExpanded,
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return AbsorbPointer(
-      absorbing: !isExpanded,
-      child: GestureDetector(
-        onTap: _onTap,
-        child: Container(
-          width: double.infinity,
-          margin: const EdgeInsets.only(
-            bottom: 20,
-          ),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            color: bgColor,
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 30,
-              vertical: 20,
+    return Material(
+      type: MaterialType.transparency,
+      child: AbsorbPointer(
+        absorbing: !widget.isExpanded,
+        child: GestureDetector(
+          onTap: _onTap,
+          child: Container(
+            width: double.infinity,
+            margin: const EdgeInsets.only(
+              bottom: 20,
             ),
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 100,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          'VIP CARD',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: colors[widget.index],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 30,
+                vertical: 20,
+              ),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 100,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            'VIP CARD',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
                           ),
-                        ),
-                        Text(
-                          '**** **** **** **88',
-                          style: TextStyle(
-                            color: Colors.white,
+                          Text(
+                            '**** **** **** **88',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                      Stack(
+                        clipBehavior: Clip.none,
+                        children: [
+                          Positioned(
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.red,
+                              ),
+                            ),
                           ),
-                        )
-                      ],
+                          Positioned(
+                            right: 20,
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.amber,
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CardDetailScreen extends StatelessWidget {
+  const CardDetailScreen({
+    super.key,
+    required this.index,
+    required this.isExpanded,
+  });
+
+  final int index;
+  final bool isExpanded;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Transactions'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Hero(
+              tag: index,
+              child: CreditCard(
+                index: index,
+                isExpanded: isExpanded,
+              ),
+            ),
+            ...[
+              for (var i in [1, 1, 1, 1, 1, 1])
+                Container(
+                  margin: const EdgeInsets.only(bottom: 8),
+                  child: ListTile(
+                    tileColor: Colors.grey.shade100,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    Stack(
-                      clipBehavior: Clip.none,
-                      children: [
-                        Positioned(
-                          child: Container(
-                            width: 50,
-                            height: 50,
-                            decoration: const BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.red,
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                          right: 20,
-                          child: Container(
-                            width: 50,
-                            height: 50,
-                            decoration: const BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.amber,
-                            ),
-                          ),
-                        )
-                      ],
-                    )
-                  ],
+                    leading: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.red,
+                      ),
+                      child: const Icon(
+                        Icons.shopping_bag,
+                        color: Colors.white,
+                      ),
+                    ),
+                    title: const Text(
+                      'Uniqlo',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    subtitle: Text(
+                      'Gangnam Branch',
+                      style: TextStyle(
+                        color: Colors.grey.shade800,
+                      ),
+                    ),
+                    trailing: const Text(
+                      "\$482,895",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                )
+            ]
+                .animate(
+                  interval: 500.milliseconds,
+                )
+                .fadeIn(begin: 0)
+                .flipV(
+                  begin: -0.5,
+                  end: 0,
+                  curve: Curves.bounceOut,
                 ),
-              ],
-            ),
-          ),
+          ],
         ),
       ),
     );
